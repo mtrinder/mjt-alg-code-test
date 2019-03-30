fc4b193bedc513bb067a035b6a17b74d899a86ab
Mark Trinder <mtrinder@geomatic.com.au>
2019-03-30 14:49:00 +1100

    Refactored UI to use bootstrap and font-awesome. Added header/footer/consume components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ConsumeComponent } from './consume/consume.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ConsumeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    TabsModule.forRoot(),
     AlertModule.forRoot()
   ],
   providers: [],
---
+++ src/app/consume/consume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consume',
+  templateUrl: './consume.component.html',
+  styleUrls: ['./consume.component.scss']
+})
+export class ConsumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
